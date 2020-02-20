f9576c8a35c820e1b5133c67f79e4831250a0ec6
tarasvivcharenko <taras.vivcharenko@gmail.com>
2020-02-20 13:06:11 +0200

    add new file

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
     <title>Travel More</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="<?php bloginfo('tamplate_url')?>/css/style.css">
-    <link rel="stylesheet" href="<?php bloginfo('tamplate_url')?>/css/normalize.css">
-    <link rel="stylesheet" href="<?php bloginfo('tamplate_url')?>/css/reset.css">
-    <link rel="shortcut icon" type="image/png" href="<?php bloginfo('tamplate_url')?>/favicon.png">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/style.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_url')?>/css/reset.css">
+    <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_url')?>/favicon.png">
     <link href="https://fonts.googleapis.com/css?family=Aleo:300,400,700|Josefin+Sans:100,300,400,600,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" integrity="sha384-KA6wR/X5RY4zFAHpv/CnoG2UW1uogYfdnP67Uv7eULvTveboZJg0qUpmJZb5VqzN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -133,7 +133,6 @@
                 <div class="col-lg-4 col-md-4 col-sm-12 col-12 d-flex flex-column justify-content-center align-items-center leftBlockWhatWeDo">
                     <div class="backgroundIcon d-flex flex-column justify-content-center align-items-center">
                         <div class="iconWhatWeDo">
-                            <?xml version="1.0" encoding="utf-8"?>
                             <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
                             "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
                             <svg version="1.1" xmlns="http://www.w3.org/2000/svg"
