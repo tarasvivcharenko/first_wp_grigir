7a463f4ade71c1f25dedf83c59890cebc14c72cc
tarasvivcharenko <taras.vivcharenko@gmail.com>
2020-02-20 14:36:09 +0200

    Restructuring of the site

---
+++ template-parts/heroSection.php
@@ -0,0 +1,49 @@
+<section class="hero" id="heroSection">
+    <div class="container containerHero">
+        <div class="row text-center">
+            <div class="col-lg-12 col-sm-12 col-12">
+                <h1 class="titleHero">BHROMAON</h1>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-12 col-sm-12 col-12">
+                <h3 class="descriptionHero">Safe Travel Anywhere</h3>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10 col-sm-10 col-10 text-center">
+                <form class="inputForm" action="#" method="get" id="form">
+                    <input class="formInputHero" autocomplete="off" type="text" name="search" id="search" placeholder="Search Your Travel Destination">
+                    <button class="formButtonHero" type="submit" form="form"><span>SEARCH</span>
+                        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+                        "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+                        <svg class="search" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                             xmlns:xlink="http://www.w3.org/1999/xlink" width="20" height="20"
+                             viewBox="0 0 32 32">
+                            <g id="icomoon-ignore">
+                            </g>
+                            <path d="M28.591 27.273l-7.263-7.264c1.46-1.756 2.339-4.010 2.339-6.471 0-5.595-4.535-10.129-10.129-10.129-5.594 0-10.129 4.535-10.129 10.129 0 5.594 4.536 10.129 10.129 10.129 2.462 0 4.716-0.879 6.471-2.339l7.263 7.264 1.319-1.319zM4.475 13.538c0-4.997 4.065-9.063 9.063-9.063 4.997 0 9.063 4.066 9.063 9.063s-4.066 9.063-9.063 9.063c-4.998 0-9.063-4.066-9.063-9.063z"
+                                  fill="#ffffff"></path>
+                        </svg>
+                    </button>
+                </form>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+                <div class="textScrollDownHero"><a class="textLinkScrollDownHero" href="#WhatOurTouristSay">Scroll Down</a></div>
+                <div class="iconScrollDownHero"><a class="iconLinkScrollDownHero" href="#WhatOurTouristSay">
+                        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                             width="17" height="17" viewBox="0 0 17 17">
+                            <g>
+                            </g>
+                            <path d="M16.354 5.075l-7.855 7.854-7.853-7.854 0.707-0.707 7.145 7.146 7.148-7.147 0.708 0.708z"
+                                  fill="#ffffff"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
